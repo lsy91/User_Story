1ee5c1e69ae8622f83e31787b0b657827a04b72d
이수용 <2sy.lee@quintet.co.kr>
2024-11-23 16:13:53 +0900

    [sy.lee] UI 작업 클릭한 꾸미기 이미지를 사진과 겹치는 기능 구현

--- app/src/main/java/com/example/userstory/ui/feature/photo/PhotoScreen.kt
+++ app/src/main/java/com/example/userstory/ui/feature/photo/PhotoScreen.kt
@@ -7,15 +7,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 import com.example.userstory.R
 import com.example.userstory.utils.CoilWithImageState
 
@@ -24,6 +35,10 @@ fun PhotoScreen(
     navigateToMain: () -> Unit,
 ) {
     val decoItems = List(10) { "Deco $it" } // 10개의 아이템 리스트
+
+    // TODO Test 선택된 Image Url 상태 관리
+    var selectedUrl by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -41,6 +56,31 @@ fun PhotoScreen(
                     .fillMaxSize(),
                 contentScale = ContentScale.Crop
             )
+
+            if (selectedUrl.isNotBlank()) {
+
+                // 간단히 SVG 이미지 로드
+                val imageLoader = ImageLoader.Builder(context)
+                    .components {
+                        add(SvgDecoder.Factory()) // SVG 디코더 추가
+                    }
+                    .build()
+
+                AsyncImage(
+                    model = ImageRequest.Builder(context)
+                        .data("https://firebasestorage.googleapis.com/v0/b/userstory-e9437.firebasestorage.app/o/deco_svg_images%2Fdeco_1.svg?alt=media&token=d729269f-72c0-499c-8da0-dc2621703ec1")
+                        .size(1024, 1024)
+                        .build(),
+                    contentDescription = "Loaded Image",
+                    imageLoader = imageLoader,
+//                    error = painterResource(R.drawable.error),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.Center)
+                )
+            }
+
         }
 
         Box(
@@ -67,7 +107,9 @@ fun PhotoScreen(
                     items = decoItems,
                     key = { _, demoItem -> demoItem }
                 ) {_, decoItem ->
-                    DecoItem(decoItem)
+                    DecoItem(decoItem) { url ->
+                        selectedUrl = url
+                    }
                 }
             }
         }
@@ -76,7 +118,10 @@ fun PhotoScreen(
 
 @Composable
 fun DecoItem(
-    decoItem: String
+    decoItem: String,
+    onClick: (String) -> Unit // 클릭 시 url 전달
 ) {
-    CoilWithImageState()
+    CoilWithImageState() { url ->
+        onClick(url)
+    }
 }
--- app/src/main/java/com/example/userstory/utils/AsyncImageUtils.kt
+++ app/src/main/java/com/example/userstory/utils/AsyncImageUtils.kt
@@ -1,10 +1,12 @@
 package com.example.userstory.utils
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -18,12 +20,14 @@ import coil.request.CachePolicy
 import coil.request.ImageRequest
 
 @Composable
-fun CoilWithImageState() {
+fun CoilWithImageState(
+    onImageClicked: (String) -> Unit // 클릭 시 url 을 전달하는 콜백
+) {
 
     val context = LocalContext.current
 
     // SVG 이미지 로드 state 별로 각각 동작을 처리하기 위해 AsyncImagePainter 와 Image Composable 사용
-    val decoImageUrl = "https://firebasestorage.googleapis.com/v0/b/userstory-e9437.firebasestorage.app/o/deco_svg_images%2Fdeco_1.svg?alt=media&token=45808843-f216-406b-b2e8-d5efae088265"
+    val decoImageUrl = "https://firebasestorage.googleapis.com/v0/b/userstory-e9437.firebasestorage.app/o/deco_svg_images%2Fdeco_border_1.svg?alt=media&token=097aa6dc-15b2-4608-a8fb-b4fd7e14c5a5"
 
     val imageLoader = ImageLoader.Builder(context)
         .components { add(SvgDecoder.Factory()) } // SVG 디코더 추가
@@ -34,31 +38,46 @@ fun CoilWithImageState() {
     val painter = rememberAsyncImagePainter(
         model = ImageRequest.Builder(context)
             .data(decoImageUrl)
-            .size(coil.size.Size.ORIGINAL)
-            .networkCachePolicy(CachePolicy.DISABLED) // 네트워크 요청 비활성화
+            .size(1024,1024)
+            .networkCachePolicy(CachePolicy.DISABLED) // 첫 로드 후에 네트워크 요청 비활성화
             .build(),
         imageLoader = imageLoader
     )
 
-    val state = painter.state
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .aspectRatio(1f)
-    ) {
-        when (state) {
-            is AsyncImagePainter.State.Success -> {
-                // 로드 성공
+    when (painter.state) {
+        is AsyncImagePainter.State.Success -> {
+            // 로드 성공
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(1f)
+                    .clickable {
+                        // TODO
+                        onImageClicked(decoImageUrl)
+                    }
+            ) {
                 Image(
                     painter = painter,
                     contentDescription = "Loaded",
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center),
                     contentScale = ContentScale.Crop
                 )
             }
-            is AsyncImagePainter.State.Error -> {
-                // 로드 실패
+
+        }
+        is AsyncImagePainter.State.Error -> {
+            // 로드 실패
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(1f)
+                    .clickable {
+                        // TODO
+                        onImageClicked(decoImageUrl)
+                    }
+            ) {
                 Image(
                     painter = ColorPainter(Color.Red),
                     contentDescription = "Error",
@@ -66,15 +85,15 @@ fun CoilWithImageState() {
                     contentScale = ContentScale.Crop
                 )
             }
-            else -> {
-                // 로딩 중
-                Image(
-                    painter = ColorPainter(Color.Gray),
-                    contentDescription = "Loading",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
+        }
+        else -> {
+            // 로딩 중
+            Image(
+                painter = ColorPainter(Color.Gray),
+                contentDescription = "Loading",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
